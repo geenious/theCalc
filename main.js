cb70845cc7964f8b264ae6e93a84b7d604969931
Jeff Irwin <jeff.irwin@live.com>
2017-07-14 15:23:10 -0500

    update to style, initial js commit

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+
+function calc() {
 let calculator = document.querySelector("#container");
 
-calculator.addEventListener("click", function(){
+calculator.addEventListener("click", function(evt){
   console.log("click goes the dynamite");
+  console.log(evt.target.textContent);
+  let output = document.querySelector("#output p");
+  output.textContent += evt.target.textContent;
 });
+}
+
+calc();
