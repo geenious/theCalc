b41dce653ae4759a844091261825252acd6cb56e
Jeff Irwin <jeff.irwin@live.com>
2017-07-17 13:47:24 -0500

    will solve simple equations

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 
 function calc() {
 let calculator = document.querySelector("#container");
-let num1 = 0;
-let num2 = 0;
+let num1;
+let num2;
 let operator = "";
 let equation = num1 + operator + num2;
+
+function hardClear() {
+  output.textContent = "";
+  num1 = "";
+  num2 = "";
+  operator = "";
+}
+
+function clear() {
+  output.textContent = "";
+}
 
 calculator.addEventListener("click", function(evt){
   console.log(evt);
@@ -12,22 +23,36 @@ calculator.addEventListener("click", function(evt){
 
   if (evt.target.id === "clear") {
     /*  CLEARS THE OUTPUT FIELD  */
-    output.textContent = "";
+    hardClear();
   }
   else if (evt.target.className === "operand") {
     num1 = Number(output.textContent);
+    console.log("First Number: ", num1);
     operator = evt.target.textContent;
-    console.log(num1, operator);
+    console.log("Operand: ", operator);
     output.textContent = num1;
-  }
-  else if (operator.length > 0) {
-    output.textContent = (evt.target.textContent);
-    num2 = evt.target.textContent;
-    console.log(`Our equation to solve is: ${num1} ${operator} ${num2}`);
   }
   else if (evt.target.id === "equals") {
     /*  EQUALS OUTPUT, NEEDS WORK  */
-    output.textContent = "Pizza!!";
+    if (operator === "+") {
+      output.textContent = num1 + num2;
+    }
+    else if (operator === "-") {
+      output.textContent = num1 - num2;
+    }
+    else if (operator === "X") {
+      output.textContent = num1 * num2;
+    }
+    else if (operator === "/") {
+      output.textContent = num1 / num2;
+    }
+  }
+  else if (operator.length > 0) {
+    clear();
+    output.textContent = Number(evt.target.textContent);
+    num2 = Number(output.textContent);
+    console.log("Second number: ", num2);
+    console.log(`Our equation to solve is: ${num1} ${operator} ${num2}`);
   }
   else {
   /*  PUTS ANYTHING CLICKED INTO THE OUTPUT  */
