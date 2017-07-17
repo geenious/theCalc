4df9c69e9d73ed618a5dd8b927725044bc853a7d
Jeff Irwin <jeff.irwin@live.com>
2017-07-17 10:15:59 -0500

    added variables to hold numbers and operators creating an equation to solve.

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 
 function calc() {
 let calculator = document.querySelector("#container");
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+let equation = num1 + operator + num2;
 
 calculator.addEventListener("click", function(evt){
   console.log(evt);
   let output = document.querySelector("#output");
 
   if (evt.target.id === "clear") {
+    /*  CLEARS THE OUTPUT FIELD  */
     output.textContent = "";
+  }
+  else if (evt.target.className === "operand") {
+    num1 = Number(output.textContent);
+    operator = evt.target.textContent;
+    console.log(num1, operator);
+    output.textContent = num1;
+  }
+  else if (operator.length > 0) {
+    output.textContent = (evt.target.textContent);
+    num2 = evt.target.textContent;
+    console.log(`Our equation to solve is: ${num1} ${operator} ${num2}`);
+  }
+  else if (evt.target.id === "equals") {
+    /*  EQUALS OUTPUT, NEEDS WORK  */
+    output.textContent = "Pizza!!";
   }
   else {
   /*  PUTS ANYTHING CLICKED INTO THE OUTPUT  */
-  output.textContent += evt.target.textContent;
+  output.textContent += (evt.target.textContent);
 }
 });
 }
